c40e2276fee1511ac16443cd738a20226370849b
Luís Felipe Zampieri Justo <ljusto@foursys.com.br>
2024-08-20 15:42:49 -0300

    resolvendo conflitos de merge

--- sistema-de-plataforma-de-evento/src/main/java/com/todoseventos/todos_eventos/enuns/ExceptionMessages.java
+++ sistema-de-plataforma-de-evento/src/main/java/com/todoseventos/todos_eventos/enuns/ExceptionMessages.java
@@ -15,11 +15,11 @@ public class ExceptionMessages {
     public static final String ERRO_BUSCAR_CATEGORIA_EVENTO = "Erro ao buscar categoria do evento: ";
     public static final String ERRO_BUSCAR_ENDERECO_EVENTO = "Erro ao buscar endereço do evento: ";
     public static final String EMAIL_INVALIDO = "E-mail inválido!";
-    public static final String TELEFONE_INVALIDO = "Número de celular inválido!";
+    public static final String TELEFONE_INVALIDO = "Número de telefone inválido!";
     public static final String DATA_NASCIMENTO_INVALIDA = "Data de nascimento inválida!";
-    public static final String CPF_INVALIDO = "CPF inválido! Verifiquei o dado informado.";
+    public static final String CPF_INVALIDO = "CPF inválido! Verifique o dado informado.";
     public static final String CPF_JA_CADASTRADO = "CPF já cadastrado!";
-    public static final String CNPJ_INVALIDO = "CNPJ inválido! Verifiquei o dado informado.";
+    public static final String CNPJ_INVALIDO = "CNPJ inválido! Verifique o dado informado.";
     public static final String CNPJ_JA_CADASTRADO = "CNPJ já cadastrado!";
     public static final String IDENTIFICADOR_INVALIDO = "Identificador inválido!";
     public static final String CLIENTE_NAO_ENCONTRADO = "Cliente não encontrado!";
